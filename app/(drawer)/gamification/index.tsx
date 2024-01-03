8184c976513011e3754ffb79cca56861332842be
haithamassoli <haitham.b.assoli@gmail.com>
2024-01-03 09:36:25 +0300

    quran test options screen

--- app/(drawer)/gamification/index.tsx
+++ app/(drawer)/gamification/index.tsx
@@ -1,25 +1,118 @@
 import HeaderRight from "@components/headerRight";
+import CustomButton from "@components/ui/customButton";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Colors from "@styles/colors";
 import { Box, ReText } from "@styles/theme";
-import { useNavigation } from "expo-router";
+import { SearchSchemaType, searchSchema } from "@src/types/schema";
+import { ms, vs } from "@utils/platform";
+import { router, useNavigation } from "expo-router";
+import { useForm } from "react-hook-form";
+import { StyleSheet } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import SelectDropdown from "react-native-select-dropdown";
+import ControlledInput from "@components/ui/controlledInput";
 
 const Gamification = () => {
   const navigation: any = useNavigation();
+  const { control, handleSubmit, reset, setError } = useForm<SearchSchemaType>({
+    resolver: zodResolver(searchSchema),
+  });
+
+  const onSearch = (data: SearchSchemaType) => {
+    if (isNaN(+data.search) || +data.search < 1 || +data.search > 604) {
+      setError("search", {
+        message: "يجب أن يكون البحث رقمًا بين 1 و 604",
+        type: "custom",
+        types: {
+          custom: "يجب أن يكون البحث رقمًا بين 1 و 604",
+        },
+      });
+      return;
+    }
+    reset();
+    // @ts-ignore
+    router.push(`/gamification/quran-test?page=${data.search}`);
+  };
   return (
     <Box
       flex={1}
-      paddingHorizontal="hm"
       style={{
         paddingTop: useSafeAreaInsets().top,
       }}
     >
-      <HeaderRight onPress={() => navigation.openDrawer()} />
-      <Box flex={1} justifyContent="center" alignItems="center">
-        <ReText variant="DisplaySmall">Gamification</ReText>
-        <ReText variant="BodyLarge">مجالس</ReText>
+      <Box flexDirection="row" alignItems="center" gap="hs">
+        <HeaderRight onPress={() => navigation.openDrawer()} />
+        <ReText variant="TitleMedium">اختبار الحفظ</ReText>
+      </Box>
+      <Box paddingHorizontal="hm" paddingTop="vl">
+        <ReText variant="BodyLarge" marginBottom="vxs">
+          اختبر نفسك في:
+        </ReText>
+        <SelectDropdown
+          data={["ما هي الآية التالية؟", "ترتيب الآيات"]}
+          buttonStyle={{
+            width: "100%",
+            marginBottom: vs(16),
+            height: vs(48),
+            borderRadius: ms(8),
+            backgroundColor: Colors.background,
+            borderColor: Colors.onBackground,
+            borderWidth: 0.8,
+          }}
+          buttonTextStyle={{
+            textAlign: "left",
+            color: Colors.onBackground,
+            fontFamily: "CairoBold",
+            fontSize: ms(14),
+          }}
+          defaultButtonText="نوع الجلسة"
+          dropdownStyle={styles.dropdown}
+          rowStyle={styles.row}
+          defaultValueByIndex={0}
+          rowTextStyle={styles.rowText}
+          onSelect={(item) => console.log(item)}
+          rowTextForSelection={(item) => item}
+        />
+        <Box alignItems="center">
+          <ControlledInput
+            control={control}
+            name="search"
+            placeholder="ادحل الصفحة المطلوبة"
+            label="الصفحة"
+            mode="outlined"
+            width={"100%"}
+          />
+        </Box>
+        <CustomButton
+          title="اختبار"
+          onPress={handleSubmit(onSearch)}
+          style={{ marginTop: vs(16) }}
+          mode="contained"
+        />
       </Box>
     </Box>
   );
 };
 
 export default Gamification;
+
+const styles = StyleSheet.create({
+  dropdown: {
+    borderRadius: ms(8),
+    backgroundColor: Colors.background,
+    borderColor: Colors.onBackground,
+    borderWidth: 0.8,
+  },
+  row: {
+    width: "100%",
+    backgroundColor: Colors.background,
+    borderColor: Colors.onBackground,
+    borderWidth: 0.6,
+  },
+  rowText: {
+    fontFamily: "CairoReg",
+    fontSize: ms(14),
+    textAlign: "left",
+    color: Colors.onBackground,
+  },
+});
