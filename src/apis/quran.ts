1cf31e797a8f7911227e3b347929002d9d152df2
haithamassoli <haitham.b.assoli@gmail.com>
2024-01-03 09:34:59 +0300

    quran api

---
+++ src/apis/quran.ts
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { useStore } from "@zustand/store";
+
+export const getQuranByPage = (page: number) => {
+  return useQuery({
+    queryKey: ["quran", page],
+    queryFn: () => getQuran(page),
+    onError: (error: any) => useStore.setState({ snackbarText: error.message }),
+  });
+};
+
+const getQuran = async (page: number) => {
+  try {
+    const response = await fetch(
+      `https://api.alquran.cloud/v1/page/${page}/quran-uthmani`
+    );
+    const data = await response.json();
+    return data.data;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
