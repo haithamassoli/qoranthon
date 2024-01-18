f53e6ef22472d4f953dfc47a74f72e3a08e273a8
haithamassoli <haitham.b.assoli@gmail.com>
2024-01-18 22:32:58 +0300

    update expo SDK to 50v

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin", "expo-router/babel"],
+    plugins: [],
     env: {
       production: {
         plugins: ["transform-remove-console", "react-native-paper/babel"],
