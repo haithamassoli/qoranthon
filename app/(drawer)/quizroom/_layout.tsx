bfffee648fa10e07fb75beadb7e0c8e11146093e
haithamassoli <haitham.b.assoli@gmail.com>
2024-01-24 11:46:47 +0300

    Quiz room layout

---
+++ app/(drawer)/quizroom/_layout.tsx
@@ -0,0 +1,22 @@
+import { ms } from "@utils/platform";
+import { Stack } from "expo-router";
+
+const QuizRoomStack = () => {
+  return (
+    <Stack
+      screenOptions={{
+        headerTitleAlign: "center",
+        animation: "slide_from_left",
+        headerBackTitle: "الرجوع",
+        headerBackTitleVisible: false,
+        headerShown: false,
+        headerTitleStyle: {
+          fontFamily: "Cairo-Bold",
+          fontSize: ms(16),
+        },
+      }}
+    ></Stack>
+  );
+};
+
+export default QuizRoomStack;
---
+++ app/(drawer)/quizroom/index.tsx
@@ -0,0 +1,11 @@
+import { Box, ReText } from "@styles/theme";
+
+const QuizRoomScreen = () => {
+  return (
+    <Box>
+      <ReText variant="DisplaySmall">QuizRoomScreen</ReText>
+    </Box>
+  );
+};
+
+export default QuizRoomScreen;
